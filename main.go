7290508a7c6324a642d90824f4ca0590f5b9d2aa
wrexshop <ramos.justin21@gmail.com>
2023-10-01 11:31:50 -0700

    update

--- main.go
+++ main.go
@@ -10,5 +10,15 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello World")
+	var conferenceName = "Golang Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	// Use Println to print then move to new line
+	//fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("Welcome to the %v booking application.\n", conferenceName)
+	fmt.Printf("There are  a total of %v tickets.\n", conferenceTickets)
+	fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
 }
