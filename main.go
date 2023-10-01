35cb1bee67efb8db506fb3fd3f127033fe5327e3
wrexshop <ramos.justin21@gmail.com>
2023-10-01 11:19:04 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+	This is tutorial: https://www.youtube.com/watch?v=yyUHQIec83I&t=
+	We must first initialize the project!!
+	cmd: go init booking-app
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello World")
+}
