d569b84c609dd30ed224e87cb857e5b3f2e74f02
wrexshop <ramos.justin21@gmail.com>
2023-10-02 17:12:29 -0700

    update

--- main.go
+++ main.go
@@ -17,76 +17,143 @@ func main() {
 	//var bookings [50]string // we cannot mix the data types in the array
 	bookings := []string{}
 
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+
+	// Loop with a conditional check
+	// for remainingTickets > 0 && len(bookings) < 50
+	// for true is infiite loop is the same as for
+	for {
+		firstName, lastName, email, userTickets := getUserInput()
+
+		// The func returns 3 values
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+
+		if !isValidName || !isValidEmail || !isValidTicketNumber {
+			// We want to check all the values, hence the use of multiple ifs
+			if len(firstName) < 2 {
+				fmt.Printf("Your first name was invalid input, %v.\n", firstName)
+			}
+			if len(lastName) < 2 {
+				fmt.Printf("Your last name was invalid input, %v.\n", lastName)
+			}
+			if !isValidEmail {
+				fmt.Printf("Your email was invalid, %v.\n", email)
+			}
+			if !isValidTicketNumber {
+				fmt.Printf("The number of tickets was invalid, %v.\n", userTickets)
+			}
+
+		} else {
+			// Reduce the number of remaining tickets
+			remainingTickets = remainingTickets - userTickets
+
+			// bookings[0] = firstName+" "+lastName // adding to an array
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("The whole array: %v\n", bookings)
+			fmt.Printf("The first value: %v\n", bookings[0])
+			fmt.Printf("The array type: %T\n", bookings)
+			fmt.Printf("The array length: %v\n", len(bookings))
+
+			fmt.Printf("Thank you %v %v for booking %v tickets.\n", firstName, lastName, userTickets)
+			fmt.Printf("You will receive an email notification at %v.\n", email)
+			fmt.Printf("%v tickets remaining for the %v.\n", remainingTickets, conferenceName)
+
+			fmt.Printf("These are the first names of the attendess: %v.\n", getsFirstNames(bookings))
+
+			// How many tickets are remainging
+			if remainingTickets == 0 {
+				// end the program
+				fmt.Printf("The %v is fully booked. Come back next year!\n", conferenceName)
+				break
+			}
+		} // end of if-else statement
+
+	} // end of main loop
+
+	/*
+		city := "london"
+
+		switch city {
+		case "new york":
+			// code block
+		case "london", "berlin":
+			// same logic for london and berlin
+		case "las vegas", "phoenix":
+			// code block
+		case "mexico city":
+			// code block
+		default:
+			// handle incorrect value entered
+			fmt.Println("No valid city selected!")
+		}
+	*/
+} // end of main func
+
+func greetUsers(conferenceName string, conferenceTickets int, remainingTickets uint) {
 	// Use Println to print then move to new line
 	//fmt.Println("Welcome to", conferenceName, "booking application")
+
 	fmt.Printf("Welcome to the %v booking application.\n", conferenceName)
-	fmt.Printf("There are  a total of %v tickets.\n", conferenceTickets)
+	fmt.Printf("There are a total of %v tickets.\n", conferenceTickets)
 	fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
+}
 
-	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
+// the []string is the output and indicating its type
+func getsFirstNames(bookings []string) []string {
+	// Slice only for first names
+	firstNames := []string{}
 
-		// ask for user input
-		fmt.Println("Enter your first name:")
-		fmt.Scan(&firstName)
+	// returns two values the position and thing
+	// "range" is how we iterate through the array and slice
+	// "_" replaced the "index" as it is not used at all.
+	for _, booking := range bookings {
 
-		fmt.Println("Enter your last name:")
-		fmt.Scan(&lastName)
+		// This will split the names up. So it will have two values: first and last name
+		var full_name = strings.Fields(booking)
 
-		fmt.Println("Enter your email:")
-		fmt.Scan(&email)
+		// We will only add the first name to the firstNames slice
+		firstNames = append(firstNames, full_name[0])
+	}
+	//fmt.Printf("These are first names of bookings are: %v\n", firstNames)
+	// Instead of printing the data.  We want the main func to print the dat
+	return firstNames
+}
 
-		fmt.Println("Enter number of tickets:")
-		fmt.Scan(&userTickets)
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	// Validate first and last name. both must be atleast 2 chars
+	// both conditions must be true
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 
-		// Handle invalid from the user input
-		if userTickets > remainingTickets {
-			fmt.Printf("There are only %v tickets remaining! So you cannot book %v tickets", remainingTickets, userTickets)
-			// Force the user to fill out the information once again by using "continue"
-			continue
-		}
+	// Validate the email entered is in a correct format
+	isValidEmail := strings.Contains(email, "@")
 
-		remainingTickets = remainingTickets - userTickets
-		// bookings[0] = firstName+" "+lastName // adding to an array
-		bookings = append(bookings, firstName+" "+lastName)
+	// Validate the number of tickets. Value must be a positive numbers
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		fmt.Printf("The whole array: %v\n", bookings)
-		fmt.Printf("The first value: %v\n", bookings[0])
-		fmt.Printf("The array type: %T\n", bookings)
-		fmt.Printf("The array length: %v\n", len(bookings))
+	// Example of an OR conditional
+	//isValidCity := city == "las vegas" || city =="fallon"
+	return isValidName, isValidEmail, isValidTicketNumber
+}
 
-		fmt.Printf("Thank you %v %v for booking %v tickets.\n", firstName, lastName, userTickets)
-		fmt.Printf("You will receive an email notification at %v.\n", email)
-		fmt.Printf("%v tickets remaining for the %v.\n", remainingTickets, conferenceName)
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-		// Slice only for first names
-		firstNames := []string{}
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
 
-		// returns two values the position and thing
-		// "range" is how we iterate through the array and slice
-		// "_" replaced the "index" as it is not used at all.
-		for _, booking := range bookings {
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
 
-			// This will split the names up. So it will have two values: first and last name
-			var full_name = strings.Fields(booking)
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
 
-			// We will only add the first name to the firstNames slice
-			firstNames = append(firstNames, full_name[0])
-		}
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
 
-		fmt.Printf("These are first names of bookings are: %v\n", firstNames)
-
-		// How many tickets are remainging
-		if remainingTickets == 0 {
-			// end the program
-			fmt.Printf("The %v is fully booked. Come back next year!\n", conferenceName)
-			break
-		}
-
-	} // main for loop
-
-} // main func
+	return firstName, lastName, email, userTickets
+}
